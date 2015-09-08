abe85a1dec0216e3071ae963890b03914dd3ed37
Minh Thang <minhthang@minhs-mbp.fis.vn>
2015-09-08 13:12:55 +0700

    init

---
+++ eTesting SwiftTests/eTesting_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  eTesting_SwiftTests.swift
+//  eTesting SwiftTests
+//
+//  Created by Minh Thang on 9/8/15.
+//  Copyright © 2015 Minh Thang. All rights reserved.
+//
+
+import XCTest
+@testable import eTesting_Swift
+
+class eTesting_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
